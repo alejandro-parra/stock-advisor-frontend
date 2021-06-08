d78d1920055719c0ab411b88343a8d8b913788f7
FranciscoSnzITESM <a01634745@itesm.mx>
2021-06-07 19:52:05 -0500

    offline game working

--- src/my-worker.js
+++ src/my-worker.js
@@ -1,7 +1,8 @@
 self.addEventListener('install', function (e) {
-    console.log('install offline')
     e.waitUntil(
         caches.open('offlineFiles').then(function (cache) {
+            createCards(cache);
+            cache.add(`assets/cards/2B.svg`);
             return cache.add('offline.html');
         })
     );
@@ -9,12 +10,26 @@ self.addEventListener('install', function (e) {
 
 self.addEventListener('fetch', function (event) {
     console.log(event.request.url);
-    console.log('fetch offline')
     return event.respondWith(
-        fetch(event.request).catch(() =>
-            caches.match('offline.html').then(function (response) {
+        fetch(event.request).catch(() => {
+            if (event.request.url.includes('assets')) {
+                return caches.match(event.request).then(function (response) {
+                    return response || fetch(event.request);
+                })
+            }
+            return caches.match('offline.html').then(function (response) {
                 return response || fetch(event.request);
             })
-        )
+        })
     );
 });
+
+const createCards = (cache) => {
+    const tiers = ['H', 'D', 'C', 'S'];
+    const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'];
+    for (let value of values) {
+        for (let tier of tiers) {
+            cache.add(`assets/cards/${value}${tier}.svg`);
+        }
+    }
+};
